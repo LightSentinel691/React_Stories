09aad41a0c150a6956597ab6bcf4b74771201796
Timothy Kiige <Timothykiige98@gmail.com>
2025-06-12 17:38:11 +0300

    Refactor App component to display a list of stories with titles, authors, comments, and points

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const list = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: "Vue.js",
+      url: "https://vuejs.org/",
+      author: "Evan You",
+      num_comments: 3,
+      points: 8,
+      objectID: 3,
+    },
+    {
+      title: "Angular",
+      url: "https://angular.io/",
+      author: "Google",
+      num_comments: 6,
+      points: 9,
+      objectID: 4,
+    },
+    {
+      title: "Svelte",
+      url: "https://svelte.dev/",
+      author: "Rich Harris",
+      num_comments: 4,
+      points: 7,
+      objectID: 5,
+    },
+    {
+      title: "Next.js",
+      url: "https://nextjs.org/",
+      author: "Vercel",
+      num_comments: 5,
+      points: 10,
+      objectID: 6,
+    },
+  ];
+
+  const [stories, setStories] = useState(list);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <List list={stories} />
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+const List = ({ list }) => {
+  return (
+    <ul>
+      {list.map((item) => {
+        const { objectID, ...story } = item;
+        console.log(objectID);
+        return <Item key={objectID} story={story} />;
+      })}
+    </ul>
+  );
+};
+
+const Item = ({ story }) => {
+  return (
+    <li>
+      <span>
+        <a href={story.url}>{story.title}</a>
+      </span>{" "}
+      &nbsp;&nbsp;
+      <span>{story.author}</span> &nbsp;&nbsp;
+      <span>{story.num_comments}</span> &nbsp;&nbsp;
+      <span>{story.points}</span>
+    </li>
+  );
+};
